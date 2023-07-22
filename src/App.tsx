553570233292324260059a82e9218489ae7ad90b
sevcak <sevclukas0@gmail.com>
2023-07-22 10:00:06 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='flex flex-col items-center w-screen'>
+      <div className='text-3xl text-center w-full'>Drum Machina</div>
+      {/* Credit label */}
+      <iframe src="https://sevcak.github.io/sevcak-credit/"></iframe>
+    </div>
+  )
+}
+
+export default App;
