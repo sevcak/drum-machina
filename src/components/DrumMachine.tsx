72e362406186cc69a1ca0fd60dce32454df305e7
sevcak <sevclukas0@gmail.com>
2023-07-23 09:26:42 +0200

    feat: add more audio

--- src/components/DrumMachine.tsx
+++ src/components/DrumMachine.tsx
@@ -9,14 +9,14 @@ const DrumMachine = () => {
       {/* Drum pads */}
       <div className='grid grid-cols-3 gap-2 w-64 h-64'>
         <DrumPad trigger='Q' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='W' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='E' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='A' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='S' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='D' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='Z' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='X' sound='/audio/Heater-1.mp3' />
-        <DrumPad trigger='C' sound='/audio/Heater-1.mp3' />
+        <DrumPad trigger='W' sound='/audio/Heater-2.mp3' />
+        <DrumPad trigger='E' sound='/audio/Heater-3.mp3' />
+        <DrumPad trigger='A' sound='/audio/Heater-4.mp3' />
+        <DrumPad trigger='S' sound='/audio/Clap.mp3' />
+        <DrumPad trigger='D' sound='/audio/Open-HH.mp3' />
+        <DrumPad trigger='Z' sound='/audio/Kick_n_Hat.mp3' />
+        <DrumPad trigger='X' sound='/audio/Kick.mp3' />
+        <DrumPad trigger='C' sound='/audio/Closed-HH.mp3' />
       </div>
 
     </div>
